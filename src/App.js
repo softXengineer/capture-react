b5e10a0011290bb970b5a8feb66389f536a85957
softXengineer <mirzaqosimov2001@mail.ru>
2022-05-31 07:15:46 +0500

    Initializing about us section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <h1>Capture</h1>
+      <AboutUs />
     </div>
   );
 }
